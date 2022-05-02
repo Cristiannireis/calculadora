9a168691685a282bf35a38e5b750a75ba99735c6
Cristiane Reis <43080297+Cristiannireis@users.noreply.github.com>
2022-05-01 23:51:37 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+def calculadora():
+    #definir operadores
+    # em desenvolvimento
+    operação = input()
+
+n1 = int(input("Digite seu primeiro número"));
+n2 = int(input("Digite seu segundo número"));
+
+if operação ==  "+":
+    print(' {} + {} = {} '.format(n1, n2, s));
+    print(n1 + n2);
