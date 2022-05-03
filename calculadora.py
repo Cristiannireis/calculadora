3dc7518c4899f24bb0f87076fb033e8a2dd75787
Cristiane Reis <43080297+Cristiannireis@users.noreply.github.com>
2022-05-02 23:24:24 -0300

    Add files via upload

--- calculadora.py
+++ calculadora.py
@@ -1,11 +1,22 @@
-def calculadora():
-    #definir operadores
+
     # em desenvolvimento
-    operação = input()
 
 n1 = int(input("Digite seu primeiro número"));
 n2 = int(input("Digite seu segundo número"));
+opção = 0
 
-if operação ==  "+":
-    print(' {} + {} = {} '.format(n1, n2, s));
-    print(n1 + n2);
+while opção != 5:
+    print('''  [ 1 ] somar
+    [ 2 ] multiplicar
+    [ 3 ] maior
+    [ 4 ] outros números
+    [ 5 ] sair do programa''')
+    opção = int(input('Qual é a sua opção? '))
+
+    if opção == 1:
+        soma = n1 + n2
+        print('A soma entre {} e {}'.format(n1, n2, soma))
+    elif opção == 2:
+        produto = n1 * n2
+
+
